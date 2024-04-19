584ca47cc6bf434ec6feff20f45900013f22756c
NielsB1 <n.buijs@student.avans.nl>
2024-04-19 13:28:52 +0200

    addec cvtest

---
+++ OpenCvTest.cpp
@@ -0,0 +1,29 @@
+// OpenCvTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#ifdef _DEBUG
+#pragma comment(lib, "opencv_world490d")
+#else
+#pragma comment(lib, "opencv_world490")
+#endif
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    auto img = cv::imread("test.jpeg");
+    cv::imshow("Monkey Man", img);
+    cv::waitKey();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+z
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
