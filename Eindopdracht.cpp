ea65e736fd255d2fad9840283162fde266d28974
NielsB1 <n.buijs@student.avans.nl>
2024-05-20 10:57:20 +0200

    eindopdracht begin

--- Eindopdracht.cpp
+++ Eindopdracht.cpp
@@ -8,39 +8,67 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
+// #include <experimental/filesystem>
 
 using namespace cv;
 using namespace std;
 
 
-void main() {
-
+void main()
+{
 	Mat img;
 	VideoCapture cap(0);
 
 	CascadeClassifier plateCascade;
-	plateCascade.load("kenteken.xml");
+	plateCascade.load("Resources/haarcascade_russian_plate_number.xml");
 
 	if (plateCascade.empty()) { cout << "File not loaded" << endl; }
 
 	vector<Rect> plates;
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
 
-	while (true) {
-
+	int count = 0;
+	while (true)
+	{
 		cap.read(img);
 		plateCascade.detectMultiScale(img, plates, 1.1, 10);
 
-		for (int i = 0; i < plates.size(); i++)
+		for (int i = 0; i < static_cast<int>(plates.size()); i++)
 		{
 			Mat imgCrop = img(plates[i]);
-			//imshow(to_string(i), imgCrop);
-			imwrite("Resources/Plates/" + to_string(i) + ".png", imgCrop);
-			rectangle(img, plates[i].tl(), plates[i].br(), Scalar(255, 0, 255), 3);
 
+			Mat cannyImg, erodeImg, grayImg, rangeImg;
+			cvtColor(imgCrop, grayImg, COLOR_BGR2GRAY);
+			inRange(grayImg, Scalar(40.0, 40.0, 40.0), Scalar(90.0, 90.0, 90.0), rangeImg);
+			Canny(rangeImg, cannyImg, 40, 60);
 			
+			// imshow("Image", rangeImg);
+			// imshow("Image", cannyImg);
+			// waitKey();
+
+			findContours(cannyImg, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+			for (const vector<Point>& contour : contours)
+			{
+				double area = contourArea(contour);
+				Rect rect = boundingRect(contour);
+				double aspectRatio = static_cast<double>(rect.width) / rect.height;
+				if (area > 200.0 && aspectRatio < 1.0)
+				{
+					Mat cropImg;
+					cropImg = imgCrop(rect);
+					// imshow("Cropped Image", cropImg);
+					imwrite("Resources/Plates/" + to_string(count) + ".png", cropImg);
+					cout << area << " | img: " << count << " | aspectRatio: " << aspectRatio << '\n';
+					count++;
+				}
+			}
+
+			rectangle(img, plates[i].tl(), plates[i].br(), Scalar(255, 0, 255), 3);
 		}
 
 		imshow("Image", img);
-		waitKey(1);
+		waitKey(100);
 	}
 }
