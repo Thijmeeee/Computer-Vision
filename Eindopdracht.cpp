c1a165f16c6ff60824b654a1a430575d970fbcd2
Thijmeeee <DoeKanaalDigitaal@gmail.com>
2024-05-28 10:02:55 +0200

    implemented sorting function

--- Eindopdracht.cpp
+++ Eindopdracht.cpp
@@ -1,5 +1,3 @@
-// OpenCvTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 #ifdef _DEBUG
 #pragma comment(lib, "opencv_world490d")
 #else
@@ -8,11 +6,14 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
-// #include <experimental/filesystem>
+#include <algorithm>
 
 using namespace cv;
 using namespace std;
 
+bool sortByX(const Rect& a, const Rect& b) {
+	return a.x < b.x;
+}
 
 void main()
 {
@@ -42,13 +43,10 @@ void main()
 			cvtColor(imgCrop, grayImg, COLOR_BGR2GRAY);
 			inRange(grayImg, Scalar(40.0, 40.0, 40.0), Scalar(90.0, 90.0, 90.0), rangeImg);
 			Canny(rangeImg, cannyImg, 40, 60);
-			
-			// imshow("Image", rangeImg);
-			// imshow("Image", cannyImg);
-			// waitKey();
 
 			findContours(cannyImg, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
+			vector<Rect> boundingBoxes;
 			for (const vector<Point>& contour : contours)
 			{
 				double area = contourArea(contour);
@@ -56,13 +54,36 @@ void main()
 				double aspectRatio = static_cast<double>(rect.width) / rect.height;
 				if (area > 200.0 && aspectRatio < 1.0)
 				{
-					Mat cropImg;
-					cropImg = imgCrop(rect);
-					// imshow("Cropped Image", cropImg);
-					imwrite("Resources/Plates/" + to_string(count) + ".png", cropImg);
-					cout << area << " | img: " << count << " | aspectRatio: " << aspectRatio << '\n';
-					count++;
+					boundingBoxes.push_back(rect);
 				}
+			}
+
+			// Sorteer de bounding boxes van links naar rechts
+			sort(boundingBoxes.begin(), boundingBoxes.end(), sortByX);
+
+			// Verwijder dubbele bounding boxes
+			boundingBoxes.erase(unique(boundingBoxes.begin(), boundingBoxes.end()), boundingBoxes.end());
+
+
+			for (int i = boundingBoxes.size() - 1; i >= 0; i--)
+			{
+				for (int j = i - 1; j >= 0; j--)
+				{
+					if (abs(boundingBoxes[i].x - boundingBoxes[j].x) < 5)
+					{
+						boundingBoxes.erase(boundingBoxes.begin() + j);
+						i--; 
+					}
+				}
+			}
+
+
+			for (const Rect& rect : boundingBoxes)
+			{
+				Mat cropImg = imgCrop(rect);
+				imwrite("Resources/Plates/" + to_string(count) + ".png", cropImg);
+				cout << "img: " << count << " | rect: " << rect << '\n';
+				count++;
 			}
 
 			rectangle(img, plates[i].tl(), plates[i].br(), Scalar(255, 0, 255), 3);
